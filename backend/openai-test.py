f8d6b4d30cd76e4b3618755130bbbcbc533b865b
cameronmcnz <you@example.com>
2024-04-30 10:05:24 -0400

    'brains'

--- backend/openai-test.py
+++ backend/openai-test.py
@@ -1,5 +1,5 @@
 from openai import OpenAI
-client = OpenAI(api_key='sk-proj-tsi0YJqA2CtjvpZsJsF8T3BlbkFJZcvVcqynO3GzG2FyXzkF')
+
 
 completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
